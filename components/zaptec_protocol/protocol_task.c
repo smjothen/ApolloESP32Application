ce767c59f0175419286dddaed8d94beebfaf2a9e
Arnt Lima <aul@datarespons.no>
2020-03-17 08:35:58 +0100

    restructure uart recv

--- components/zaptec_protocol/protocol_task.c
+++ components/zaptec_protocol/protocol_task.c
@@ -2,6 +2,8 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/xtensa_config.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
 
@@ -11,17 +13,25 @@
 #define TAG __FILE__
 
 void uartCommsTask(void *pvParameters);
+void uartRecvTask(void *pvParameters);
 void configureUart();
 void onCharRx(char c);
+
+SemaphoreHandle_t uart_write_lock;
+QueueHandle_t uart_recv_message_queue;
+QueueHandle_t uart0_events_queue;
 
 const int uart_num = UART_NUM_2;
 
 void zaptecProtocolStart(){
     ESP_LOGI(TAG, "starting protocol task");
     static uint8_t ucParameterToPass = {0};
+    TaskHandle_t uartRecvTaskHandle = NULL;
     TaskHandle_t taskHandle = NULL;
     int stack_size = 4096;
+    xTaskCreate( uartRecvTask, "uartRecvTask", stack_size, &ucParameterToPass, 5, &uartRecvTaskHandle );
     xTaskCreate( uartCommsTask, "UARTCommsTask", stack_size, &ucParameterToPass, 5, &taskHandle );
+    configASSERT(uartRecvTaskHandle);
     configASSERT( taskHandle );
     if( taskHandle == NULL ){
         ESP_LOGE(TAG, "failed to start task");
@@ -29,9 +39,84 @@ void zaptecProtocolStart(){
     
 }
 
+ZapMessage runRequest(const uint8_t *encodedTxBuf, uint length){
+
+    if( xSemaphoreTake( uart_write_lock, portMAX_DELAY ) == pdTRUE )
+    {
+    
+        uart_flush(uart_num);
+        xQueueReset(uart_recv_message_queue);
+
+        uart_write_bytes(uart_num, (char *)encodedTxBuf, length);
+
+        ZapMessage rxMsg;
+        xQueueReceive( 
+            uart_recv_message_queue,
+            &( rxMsg ),
+            portMAX_DELAY
+        );
+
+        // dont release uart_write_lock, let caller use freeZapMessageReply()
+        return rxMsg;
+    }
+    configASSERT(false);
+    ZapMessage dummmy_reply = {0};
+    return dummmy_reply;
+}
+
+void freeZapMessageReply(){
+    xSemaphoreGive(uart_write_lock) ;
+}
+
+void uartRecvTask(void *pvParameters){
+    uart_write_lock = xSemaphoreCreateMutex();
+    configASSERT(uart_write_lock);
+    uart_recv_message_queue = xQueueCreate( 1, sizeof( ZapMessage ) );
+    configASSERT(uart_recv_message_queue);
+
+    configureUart();
+
+    ZapMessage rxMsg;
+    uint8_t uart_data_size = 128;
+    uint8_t uart_data[uart_data_size];
+
+    uart_event_t event;
+
+    while(true)
+        {
+            configASSERT(xQueueReceive(uart0_events_queue, (void * )&event, (portTickType)portMAX_DELAY))
+
+            if(event.type != UART_DATA){continue;}
+
+            if(uxSemaphoreGetCount(uart_write_lock)==1){
+                ESP_LOGE(TAG, "got uart data without outstanding request");
+                continue;
+            }
+
+            configASSERT(event.size <= uart_data_size);
+            int length = uart_read_bytes(uart_num, uart_data, event.size, portMAX_DELAY);
+
+            ESP_LOGI(TAG, "feeding %d bytes to ZParseFrame:", length);
+
+            for(int i = 0; i<length; i++){
+                uint8_t rxByte = uart_data[i];
+
+                if(ZParseFrame(rxByte, &rxMsg))
+                {   
+                    uart_flush(uart_num);
+                    configASSERT(xQueueSend(
+                        uart_recv_message_queue,                        
+                        ( void * ) &rxMsg,
+                        portMAX_DELAY
+                    ))
+                    printf("handling frame\n\r");
+                }
+            }
+        }
+}
+
 void uartCommsTask(void *pvParameters){
     ESP_LOGI(TAG, "configuring uart");
-    configureUart();
 
     while (true)
     {
@@ -52,56 +137,17 @@ void uartCommsTask(void *pvParameters){
         );
 
         ESP_LOGI(TAG, "sending zap message, %d bytes", encoded_length);
+        
+        ZapMessage rxMsg = runRequest(encodedTxBuf, encoded_length);
+        printf("frame type: %d \n\r", rxMsg.type);
+        printf("frame identifier: %d \n\r", rxMsg.identifier);
+        printf("frame timeId: %d \n\r", rxMsg.timeId);
 
-        uart_flush(uart_num);
-        uart_write_bytes(uart_num, (char *)encodedTxBuf, encoded_length);
+        uint8_t error_code = ZDecodeUInt8(rxMsg.data);
+        printf("frame error code: %d\n\r", error_code);
+        freeZapMessageReply();
 
-        ZapMessage rxMsg;
-        bool reply_pending = true;
-        uint32_t empty_uart_count = 0;
-        while(reply_pending)
-        {
-            if(empty_uart_count>15){
-                ESP_LOGW(TAG, "missed reply from dsPIC");
-                break;
-            }
-
-            uint8_t uart_data[128];
-            int length = uart_read_bytes(uart_num, uart_data, 128, 0);
-
-            if(length == 0){
-                ESP_LOGI(TAG, "got no reply bytes");
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                empty_uart_count++;
-                continue;
-            }else{
-                empty_uart_count = 0;
-            }
-
-            ESP_LOGI(TAG, "feeding %d bytes to ZParseFrame:", length);
-
-            for(int i = 0; i<length; i++){
-                uint8_t rxByte = uart_data[i];
-                // ESP_LOGI(TAG, "eating byte: %d", rxByte);
-
-                if(ZParseFrame(rxByte, &rxMsg))
-                {   
-                    uart_flush(uart_num);
-                    reply_pending = false;
-                    printf("handling frame\n\r");
-                    printf("frame type: %d \n\r", rxMsg.type);
-                    printf("frame identifier: %d \n\r", rxMsg.identifier);
-                    printf("frame timeId: %d \n\r", rxMsg.timeId);
-
-                    uint8_t error_code = ZDecodeUInt8(rxMsg.data);
-                    printf("frame error code: %d\n\r", error_code);
-                }
-            
-            }
-            
-        }
-
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     
 }
@@ -120,16 +166,11 @@ void configureUart(){
     };
 
     const int uart_buffer_size = (1024 * 2);
-    QueueHandle_t uart_queue;
-    // Install UART driver using an event queue here"
+
     ESP_ERROR_CHECK(uart_driver_install(uart_num, uart_buffer_size, \
-        uart_buffer_size, 10, &uart_queue, 0));
+        uart_buffer_size, 20, &uart0_events_queue, 0));
 
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));    
 
     ESP_ERROR_CHECK(uart_set_pin(uart_num, tx_pin, rx_pin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-}
-
-void onCharRx(char c){
-    //ESP_LOGI(TAG, "got char: %c", c);
 }
