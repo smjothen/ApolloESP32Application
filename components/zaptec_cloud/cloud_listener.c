2581c17db0f17cea3681b0ddf97084246adfa3e7
Vegard Valebjørg <vv@zaptec.com>
2020-09-21 14:19:04 +0200

    Try fix on repeated MQTT_EVENT_ERROR

--- components/zaptec_cloud/cloud_listener.c
+++ components/zaptec_cloud/cloud_listener.c
@@ -295,10 +295,18 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 
     	ESP_LOGI(TAG, "MQTT_EVENT_ERROR: %d/10", resetCounter);
 
+        if(resetCounter == 5)
+        {
+        	esp_err_t rconErr = esp_mqtt_client_reconnect(mqtt_client);
+        	ESP_LOGI(TAG, "MQTT event reconnect! Error: %d", rconErr);
+        }
+
         if(resetCounter == 10)
         {
+        	ESP_LOGI(TAG, "MQTT_EVENT_ERROR restart");
         	esp_restart();
         }
+
 
         break;
     default:
