7da025b383fbac833521ca50e20cd94e24d81928
Arnt Lima <aul@zaptec.com>
2021-03-05 15:08:44 +0100

    write segmented ota to flash

--- components/apollo_ota/segmented_ota.c
+++ components/apollo_ota/segmented_ota.c
@@ -1,7 +1,9 @@
 #include "segmented_ota.h"
+#include "esp_ota_ops.h"
 
 #include "stdio.h"
 #include "string.h"
+#include "freertos/task.h"
 
 #include "ota_log.h"
 #include "esp_log.h"
@@ -9,6 +11,8 @@
 static const char *TAG = "segmented_ota";
 
 int total_size = 0;
+esp_ota_handle_t update_handle = { 0 };
+
 
 extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
 
@@ -34,8 +38,12 @@ static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     case HTTP_EVENT_ON_DATA:
         ESP_LOGI(TAG, "got ota data, %d bytes", evt->data_len);
         int * error_p  = (int * )evt->user_data;
-        *error_p = 3;
-        ota_log_download_progress_debounced(evt->data_len);
+        esp_err_t err = esp_ota_write(update_handle, evt->data, evt->data_len);
+        if(err!=ESP_OK){
+            ESP_LOGE(TAG, "Writing data to flash failed");
+            *error_p = 3;
+        }
+
         break;
     case HTTP_EVENT_ON_FINISH:
         ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
@@ -49,7 +57,15 @@ static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 
 void do_segmented_ota(char *image_location){
     ESP_LOGW(TAG, "running experimental segmented ota");
-    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+    
+    esp_err_t err = esp_ota_begin(
+        esp_ota_get_next_update_partition(NULL),
+         OTA_SIZE_UNKNOWN, &update_handle
+    );
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "esp_ota_begin failed (%s)", esp_err_to_name(err));
+        return;
+    }
 
     int read_start=0;
     int chunk_size = 65536;
@@ -69,14 +85,16 @@ void do_segmented_ota(char *image_location){
 
     while(true){
         if((total_size > 0 )&&(read_start>total_size)){
-            ESP_LOGW(TAG, "Flashing all segments done");
+            ESP_LOGW(TAG, "Flashing all segments done, proceeding to validation");
             break;
         }
 
         esp_http_client_handle_t client = esp_http_client_init(&config);
 
         char range_header_value[64];
-        snprintf(range_header_value, 64, "bytes=%d-%d", read_start, read_end);
+        // standars compliant servers take the end range as inclusive, our server currently does not,
+        // thus we read one byte paste the last value we actually want
+        snprintf(range_header_value, 64, "bytes=%d-%d", read_start, read_end+1);
         esp_http_client_set_header(client, "Range", range_header_value);
 
         ESP_LOGI(TAG, "fetching [%s]", range_header_value);
@@ -88,11 +106,12 @@ void do_segmented_ota(char *image_location){
         }
         esp_http_client_cleanup(client);
 
-        //TODO: if error continue;
         if(flash_error > 0){
             ESP_LOGW(TAG, "error when flashing segment, retrying");
-            //continue;
+            continue;
         }
+
+        ESP_LOGI(TAG, "Flashed %d tot %d of %d", read_start, read_end, total_size);
 
         read_start = read_end + 1;
         read_end = read_start + chunk_size;
@@ -102,7 +121,14 @@ void do_segmented_ota(char *image_location){
         }
     }
     
-    ESP_LOGW(TAG, "TODO mark partition");
+    esp_err_t end_err = esp_ota_end(update_handle);
+    if(end_err!=ESP_OK){
+        ESP_LOGE(TAG, "Partition validation error %d", end_err);
+    }else{
+        ESP_LOGW(TAG, "update complete, rebooting soon");
+    }
+    vTaskDelay(pdMS_TO_TICKS(3000));
+    esp_restart();
 
 
 }
