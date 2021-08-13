89738fea56de1b928a0bddc2c03c38eb6931d0f5
Vegard Valebjørg <vv@zaptec.com>
2021-08-13 11:47:28 +0200

    Included MCU version which uses weld-detect pin to trig relay faults.

--- main/main.c
+++ main/main.c
@@ -44,7 +44,7 @@ const char *TAG_MAIN = "MAIN     ";
 #define GPIO_OUTPUT_DEBUG_PIN_SEL (1ULL<<GPIO_OUTPUT_DEBUG_LED)
 
 uint32_t onTimeCounter = 0;
-char softwareVersion[] = "0.0.1.11";
+char softwareVersion[] = "0.0.1.12";
 
 uint8_t GetEEPROMFormatVersion()
 {
