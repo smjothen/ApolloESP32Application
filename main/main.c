3f8bb020cbd3af02f242b3d2ee0ceaa66299c476
Vegard Valebjørg <vv@zaptec.com>
2021-12-15 12:33:55 +0100

    Set proper version number. Removed diagnostics printout of IT3 currents.

--- main/main.c
+++ main/main.c
@@ -46,7 +46,7 @@ const char *TAG_MAIN = "MAIN     ";
 #define GPIO_OUTPUT_DEBUG_PIN_SEL (1ULL<<GPIO_OUTPUT_DEBUG_LED)
 
 uint32_t onTimeCounter = 0;
-char softwareVersion[] = "0.0.0.92";
+char softwareVersion[] = "0.0.3.2";
 
 uint8_t GetEEPROMFormatVersion()
 {
