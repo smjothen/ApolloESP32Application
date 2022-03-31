894bd5fa57203f02b4c06a2cf1b76a5063d4b517
Vegard Valebjørg <vv@zaptec.com>
2022-03-31 13:56:06 +0200

    Added files for OfflineHandling

---
+++ main/offlineHandler.c
@@ -0,0 +1,241 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "offlineHandler.h"
+#include "sessionHandler.h"
+#include "string.h"
+#include "chargeSession.h"
+#include "../components/zaptec_protocol/include/zaptec_protocol_serialisation.h"
+#include "../components/zaptec_protocol/include/protocol_task.h"
+#include "zaptec_cloud_listener.h"
+#include "zaptec_cloud_observations.h"
+#include "storage.h"
+
+
+static const char *TAG = "OFFLINEHANDLER ";
+
+#define PING_REPLY_TIMER_LIMIT_TO_OFFLINE 30 							 //TODO evaluate 100
+#define PING_REPLY_CHECK_INCHARGE_PERIODE 60 							 //TODO evaluate 100
+#define PING_REPLY_TIMER_INCREASE (PING_REPLY_CHECK_INCHARGE_PERIODE/5) //TODO evaluate 100
+
+
+static enum PingReplyState pingReplyState = PING_REPLY_ONLINE;
+static uint32_t pingReplyTimerToOffline = 0;
+static uint32_t pingReplyTimerToOnline = 0;
+static uint32_t incrementingToOnlineLimit = PING_REPLY_CHECK_INCHARGE_PERIODE;
+void offlineHandler_CheckPingReply()
+{
+	/// Check if we are waiting for the Cloud-command
+	if(pingReplyState == PING_REPLY_AWAITING_CMD)
+	{
+		pingReplyTimerToOffline++;
+
+		/// No cloud command received - start offline charging and do InChargePing periodically
+		if(pingReplyTimerToOffline == PING_REPLY_TIMER_LIMIT_TO_OFFLINE)//TODO: define timeout
+		{
+			offlineHandler_UpdatePingReplyState(PING_REPLY_OFFLINE);
+		}
+	}
+	else
+	{
+		pingReplyTimerToOffline = 0;
+	}
+
+	/// Check if InCharge has recovered and we should go back to online mode
+	if(pingReplyState == PING_REPLY_OFFLINE)
+	{
+		pingReplyTimerToOnline++;
+
+		/// Repeatedly, with lower interval, send pulse to try and get InCharge response
+		if(pingReplyTimerToOnline % incrementingToOnlineLimit == 0)
+		{
+			/// Don't allow longer check period than 1 hour - TODO evaluate
+			if(incrementingToOnlineLimit <= 3600)
+				incrementingToOnlineLimit += PING_REPLY_TIMER_INCREASE;
+
+			ESP_LOGW(TAG, "Pinging InCharge! - New incrementingToOnlineLimit: %d", incrementingToOnlineLimit);
+
+			pingReplyTimerToOnline = 0;
+
+			update_mqtt_event_pattern(true);
+			//publish_cloud_pulse();
+			publish_debug_telemetry_observation_RequestNewStartChargingCommand();
+			update_mqtt_event_pattern(false);
+		}
+	}
+	else
+	{
+		pingReplyTimerToOnline = 0;
+		incrementingToOnlineLimit = PING_REPLY_CHECK_INCHARGE_PERIODE;
+	}
+}
+
+void offlineHandler_UpdatePingReplyState(enum PingReplyState newState)
+{
+	char buf[50] = {0};
+	char *pbuf = buf;
+
+	//Debug, for printing out the state change to console
+	for (int i = 0; i < 2; i++)
+	{
+		if(pingReplyState == PING_REPLY_ONLINE)
+			sprintf(pbuf, "PING_REPLY_ONLINE");
+		else if(pingReplyState == PING_REPLY_AWAITING_CMD)
+			sprintf(pbuf, "PING_REPLY_AWAITING_CMD");
+		else if(pingReplyState == PING_REPLY_CMD_RECEIVED)
+			sprintf(pbuf, "PING_REPLY_CMD_RECEIVED");
+		else if(pingReplyState == PING_REPLY_OFFLINE)
+			sprintf(pbuf, "PING_REPLY_OFFLINE");
+
+		if(i==0)
+		{
+			sprintf(pbuf+strlen(buf), " -> ");
+			int len = strlen(buf);
+			pbuf= &buf[len];
+			pingReplyState = newState;
+		}
+	}
+
+	ESP_LOGW(TAG, "Updating PingReplyState: %s", buf);
+}
+
+enum PingReplyState offlineHandler_GetPingReplyState()
+{
+	return pingReplyState;
+}
+
+
+bool offlineHandler_IsPingReplyOffline()
+{
+	return (pingReplyState == PING_REPLY_OFFLINE);
+}
+
+static uint32_t simulateOfflineTimeout = 180;
+static bool simulateOffline = false;
+void offlineHandler_SimulateOffline(int offlineTime)
+{
+	simulateOffline = true;
+	simulateOfflineTimeout = offlineTime;
+}
+
+
+void offlineHandler_CheckForSimulateOffline()
+{
+	//Allow simulating timelimited offline mode initated with cloud command
+	if(simulateOffline == true)
+	{
+		//Override state
+		//isOnline = false;
+		MqttSetSimulatedOffline(true);
+
+		simulateOfflineTimeout--;
+		if(simulateOfflineTimeout == 0)
+		{
+			simulateOffline= false;
+			MqttSetSimulatedOffline(false);
+		}
+	}
+}
+
+
+static bool requestCurrentWhenOnline = false;
+
+bool offlineHandler_IsRequestingCurrentWhenOnline()
+{
+	return requestCurrentWhenOnline;
+}
+
+void offlineHandler_SetRequestingCurrentWhenOnline(bool state)
+{
+	requestCurrentWhenOnline = state;
+}
+
+
+/// The main function where offline current is granted if command from Cloud is not received within time
+static bool offlineCurrentSent = false;
+void offlineHandler_CheckForOffline()
+{
+
+	int activeSessionId = strlen(chargeSession_GetSessionId());
+	uint8_t chargeOperatingMode = MCU_GetChargeOperatingMode();
+
+	//Handle charge session started offline
+	if((activeSessionId > 0) && (chargeOperatingMode == CHARGE_OPERATION_STATE_REQUESTING))//2 = Requesting, add definitions
+	{
+		//Wait until a valid tag is registered.
+		if((storage_Get_AuthenticationRequired() == 1) && (chargeSession_Get().AuthenticationCode[0] == '\0'))
+			return;
+
+		requestCurrentWhenOnline = true;
+
+		MessageType ret = MCU_SendCommandId(CommandAuthorizationGranted);
+		if(ret == MsgCommandAck)
+		{
+			ESP_LOGI(TAG, "Offline MCU Granted command OK");
+
+			float offlineCurrent = storage_Get_DefaultOfflineCurrent();
+
+			MessageType ret = MCU_SendFloatParameter(ParamChargeCurrentUserMax, offlineCurrent);
+			if(ret == MsgWriteAck)
+			{
+				MessageType ret = MCU_SendCommandId(CommandStartCharging);
+				if(ret == MsgCommandAck)
+				{
+					ESP_LOGI(TAG, "Offline MCU Start command OK: %fA", offlineCurrent);
+				}
+				else
+				{
+					ESP_LOGI(TAG, "Offline MCU Start command FAILED");
+				}
+			}
+			else
+			{
+				ESP_LOGE(TAG, "Offline MCU Start command FAILED");
+			}
+
+		}
+		else
+		{
+			ESP_LOGI(TAG, "Offline MCU Granted command FAILED");
+		}
+	}
+
+	/// Handle existing charge session that has gone offline
+	/// Handle charge session started offline
+	else if((activeSessionId > 0) && ((chargeOperatingMode == CHARGE_OPERATION_STATE_CHARGING) || (chargeOperatingMode == CHARGE_OPERATION_STATE_PAUSED)) && !offlineCurrentSent)//2 = Requesting, add definitions
+	{
+		float offlineCurrent = storage_Get_DefaultOfflineCurrent();
+
+		ESP_LOGI(TAG, "Sending offline current to MCU %f", offlineCurrent);
+
+		requestCurrentWhenOnline = true;
+
+		MessageType ret = MCU_SendFloatParameter(ParamChargeCurrentUserMax, offlineCurrent);
+		if(ret == MsgWriteAck)
+		{
+			MessageType ret = MCU_SendCommandId(CommandStartCharging);
+			if(ret == MsgCommandAck)
+			{
+				offlineCurrentSent = true;
+				ESP_LOGW(TAG, "### Offline MCU Start command OK: %fA ###", offlineCurrent);
+			}
+			else
+			{
+				ESP_LOGE(TAG, "Offline MCU Start command FAILED");
+			}
+		}
+		else
+		{
+			ESP_LOGE(TAG, "Offline MCU Start command FAILED");
+		}
+	}
+	else if(offlineCurrentSent == true)
+	{
+		ESP_LOGW(TAG, "Offline current mode. (SimulateOfflineTimeout: %d)", simulateOfflineTimeout);
+	}
+}
+
+void offlineHandler_ClearOfflineCurrentSent()
+{
+	offlineCurrentSent = false;
+}
