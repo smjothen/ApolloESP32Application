e7ff2546ca1dd0eee5ab0c197afe5349ded6d9a5
Vegard Valebjørg <vv@zaptec.com>
2022-04-20 11:16:35 +0200

    Commented out debug code

--- components/i2c/i2cDevices.c
+++ components/i2c/i2cDevices.c
@@ -453,11 +453,11 @@ static void i2cDevice_task(void *pvParameters)
 			humidity = SHT30ReadHumidity();
 
 			//Debug
-			struct tm readTime = {0};
+			/*struct tm readTime = {0};
 			readTime = RTCReadTime();
 			char timebuf[30];
 			strftime(timebuf, sizeof(timebuf), "%F %T", &readTime);
-			//ESP_LOGI(TAG, "Temp: %3.2fC Hum: %3.2f%%, Time is: %s", temperature, humidity, timebuf);
+			ESP_LOGI(TAG, "Temp: %3.2fC Hum: %3.2f%%, Time is: %s", temperature, humidity, timebuf);*/
 
 			RTCVerifyControlRegisters();
 		}
