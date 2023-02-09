41b1e0c31730c50172787c3898c6e5ffe8ef5c52
Eldar Hauge Torkelsen <eht@datarespons.no>
2023-02-09 13:33:57 +0100

    Changed ocpp_date_time to use utz compnent

--- components/ocpp/include/types/ocpp_date_time.h
+++ components/ocpp/include/types/ocpp_date_time.h
@@ -3,7 +3,7 @@
 
 #include <time.h>
 
-time_t ocpp_parse_date_time(const char * date_time);
+time_t ocpp_parse_date_time(char * date_time);
 int ocpp_print_date_time(time_t timestamp, char * date_time_out, size_t buffer_size);
 
 #endif /*OCPP_DATE_TIME_H*/
--- components/ocpp/types/ocpp_date_time.c
+++ components/ocpp/types/ocpp_date_time.c
@@ -2,38 +2,22 @@
 
 #include "types/ocpp_date_time.h"
 #include "types/ocpp_enum.h"
+#include "utz.h"
 
-time_t ocpp_parse_date_time(const char * date_time){
-	// the date time in a json schema is compliant with RFC 5.6 https://json-schema.org/understanding-json-schema/reference/string.html
-	// e.g. "1985-04-12T23:20:50.52Z", "1990-12-31T15:59:60-08:00"
-	// parseing based on https://stackoverflow.com/questions/26895428/how-do-i-parse-an-iso-8601-date-with-optional-milliseconds-to-a-struct-tm-in-c
+//TODO: consider adding support for full ISO standard (non utc input, duration, default fields).
 
-	int y,M,d,h,m;
-	float s;
-	int tzh = 0, tzm = 0;
+time_t ocpp_parse_date_time(char * date_time){
 
-	int paresed_arguments = sscanf(date_time, "%d-%d-%dT%d:%d:%f%d:%dZ", &y, &M, &d, &h, &m, &s, &tzh, &tzm);
+	udatetime_t dt;
+	if(utz_datetime_parse_iso(date_time, &dt) != 0)
+		return (time_t)-1;
 
-	if (6 < paresed_arguments) {
-		if (tzh < 0) {
-			tzm = -tzm;    // Fix the sign on minutes.
-		}
-	}
-
-	struct tm time;
-	time.tm_year = y - 1900; // Year since 1900
-	time.tm_mon = M - 1;     // 0-11
-	time.tm_mday = d;        // 1-31
-	time.tm_hour = h;        // 0-23
-	time.tm_min = m;         // 0-59
-	time.tm_sec = (int)s;    // 0-61 (0-60 in C++11)
-
-	//TODO: return tz and maybe handle sub-sec resolution
-	//TODO: validate and Sanity check the time
-
-	return mktime(&time);
+	return utz_datetime_to_unix(&dt);
 }
 
 int ocpp_print_date_time(time_t timestamp, char * date_time_out, size_t buffer_size){
-	return strftime(date_time_out, buffer_size, "%FT%T%z", localtime(&timestamp));
+	udatetime_t dt;
+	utz_unix_to_datetime(timestamp, &dt);
+
+	return utz_datetime_format_iso_utc(date_time_out, buffer_size, &dt);
 }
