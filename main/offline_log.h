1b03215e4cd4aa6c26b0d58331d83ca85873c455
Steve Jothen <smj@zaptec.com>
2023-03-22 12:53:54 +0100

    Incorporate some of Eldar's single mount changes from OCPP branch.

--- main/offline_log.h
+++ main/offline_log.h
@@ -3,12 +3,13 @@
 #include <stddef.h>
 #include <stdint.h>
 
+void setup_offline_log();
+
 void append_offline_energy(int timestamp, double energy);
 int attempt_log_send(void);
 
 uint32_t crc32_normal(uint32_t crc, const void *buf, size_t size);
 int deleteOfflineLog();
-
-int deleteOfflineLog();
+void offlineLog_disable(void);
 
 #endif /* OFFLINE_LOG */
