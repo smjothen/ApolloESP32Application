529bf59df48b1e0aa6ff490998d0c301d88d6af1
Vegard Valebjørg <vv@zaptec.com>
2023-10-17 16:13:38 +0200

    Corrected fw version

--- main/main.c
+++ main/main.c
@@ -60,7 +60,7 @@ static const char *TAG_MAIN = "MAIN           ";
 #define GPIO_OUTPUT_DEBUG_PIN_SEL (1ULL<<GPIO_OUTPUT_DEBUG_LED)
 
 uint32_t onTimeCounter = 0;
-char softwareVersion[] = "2.3.0.304";
+char softwareVersion[] = "2.1.0.304";
 
 uint8_t GetEEPROMFormatVersion()
 {
