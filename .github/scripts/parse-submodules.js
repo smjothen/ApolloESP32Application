42c9ca8d09102852185a18b624ff50452894fa4b
Steve Jothen <smj@zaptec.com>
2023-10-17 07:35:00 +0200

    Remove junk and test build

--- .github/scripts/parse-submodules.js
+++
@@ -1,7 +0,0 @@
-const parse = require('parse-git-config');
- 
-// sync
-console.log(parse.sync({ path: '.gitmodules' }));
- 
-// using async/await
-// (async () => console.log(await parse({ path: '.gitmodules' })))();
