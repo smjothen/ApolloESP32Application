b59aaa21150b120ce642fb59725a738c4c6e5bc8
Steve Jothen <smj@zaptec.com>
2023-10-19 11:32:05 +0200

    Update hash

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-docker pull --platform=linux/amd64 espressif/idf:v5.1.1
-docker run --platform=linux/amd64 --rm -v $PWD:/project -w /project espressif/idf:v5.1.1 idf.py build
+cd ApolloMCUApplication && \
+	./build.sh AppWithBootloader && \
+	cp out/dspic.bin ../bin && \
+	cd - && \
+	docker pull --platform=linux/amd64 espressif/idf:v5.1.1 && \
+	docker run --platform=linux/amd64 --rm -v $PWD:/project -w /project espressif/idf:v5.1.1 idf.py build
